b5a98f2f5cbda32cd018ff09dc8326a235699d8b
Tiphaine <lagadec.tiphaine@gmail.com>
2020-01-17 17:46:44 +1100

    Rewritting code with classes and objects (written: read file and plot magnets) - include draw_all_magnets file (failed before)

---
+++ Magnets/draw_all_magnets.py
@@ -0,0 +1,9 @@
+def draw_all_magnets(circular_magnets,rectangular_magnets):
+
+    for magnet in circular_magnets:
+        magnet.draw_circle()
+
+    for magnet in rectangular_magnets:
+        magnet.draw_rectangle()
+
+
